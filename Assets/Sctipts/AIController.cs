49aac5a864d2ef0e35980789e68ec6a9d518d08b
GhostDrag00n <grisha.pavlov@icloud.com>
2018-07-30 07:52:18 +0300

    AI

---
+++ Assets/Sctipts/AIController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+public class AIController : MonoBehaviour {
+
+    public GameObject player;
+    public float AttackDamage = 10f;
+    public float health = 100f;
+    public float lookRadius = 10f;
+    public float awareRadius = 1f;
+    NavMeshAgent agent;
+
+	void Start () 
+    {
+        agent = GetComponent<NavMeshAgent>();
+	}
+	
+	void Update () 
+    {
+        RaycastHit hit;
+        if (Vector3.Distance(player.transform.position, this.transform.position) <= lookRadius && Physics.Raycast(this.transform.position, player.transform.position, out hit, lookRadius))
+        {
+            agent.SetDestination(player.transform.position);
+            Attack();
+        }
+	}
+
+    void Attack()
+    {
+        if (Vector3.Distance(player.transform.position, this.transform.position) <= awareRadius)
+        {
+            player.GetComponent<PlayerController>().health -= AttackDamage;
+        }
+    }
+
+}
---
+++ Assets/Sctipts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public GameObject healthbar;
+    public float health = 100f;
+
+    private void Update()
+    {
+        healthbar.GetComponent<Image>().fillAmount = health / 100f;
+        if (health <= 0)
+        {
+            Die();   
+        }
+    }
+
+    void Die()
+    {
+        Destroy(gameObject);
+        Debug.Log("Died");
+        //adassss
+    }
+}
