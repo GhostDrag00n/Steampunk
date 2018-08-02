6e3facc1804db60b3794ce29d7d564138c74b3e4
GhostDrag00n <grisha.pavlov@icloud.com>
2018-08-02 16:03:45 +0300

    Menu

---
+++ Assets/MainManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MainManager : MonoBehaviour {
+
+    public int SceneNumb;
+
+    public void StartButton()
+    {
+        SceneManager.LoadScene(SceneNumb);
+    }
+
+}
